5bad4eaf3e8e4ffdeffc5e1fe8b8a1ef06e536c5
Rick Warling <rickwarling@gmail.com>
2017-03-07 12:10:00 +0100

    Adding comments

--- Hüs_lesson7.py
+++ Hüs_lesson7.py
@@ -9,21 +9,21 @@ if play=='x':
     print "Bye, Bye"
     exit()
 
-while True:
-    try:
+while True: #used for creating a loop(where you come back by 'continue' to this point!
+    try: #used to implement the except for a ValueError
         guess = int(raw_input("Guess a secret number between 1 and 30:\n>> " ))
         if guess == secret:
             print "You have won!!"
             print "Thank you for playing"
-            break
+            exit()
         else:
             print "Sorry you have lost, try again!"
             play = str(raw_input("press ENTER to play or type 'x' to Exit:\n>> "))
             if play == 'x':
                 print "Bye, Bye!"
                 exit()
-            continue
-    except ValueError:
+            continue #start again by while True:
+    except ValueError: #if there is a ValueError, which means when User entered a str and not an int.
         print "Oops that is not a number! Try again!"
         continue
 
